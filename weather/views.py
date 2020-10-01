572d75a3edf981698677559fc2598c510a1d22f3
Iglek Kuat <dizinnes228@gmail.com>
2020-10-01 15:10:58 +0600

    feat(weather): add parser results in index.html from views.py

--- weather/views.py
+++ weather/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 from .parser import handle_json
 
 
-def index(request):
+async def index(request):
+    results = await handle_json()
+    # print(results)
+    city_weather = {
+        'data': results['dt'],
+        'sunset': results['sunset'],
+        'temp': results['temp'],
+    }
+    print(city_weather['temp'])
     return render(request, 'weather/index.html')
