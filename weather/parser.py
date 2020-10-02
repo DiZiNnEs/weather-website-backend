35a2d6ac540f433ed02a8f44abb481a27405a728
Iglek Kuat <dizinnes228@gmail.com>
2020-10-02 17:47:13 +0600

    feat(weather): add curren_weather function

--- weather/parser.py
+++ weather/parser.py
@@ -12,15 +12,24 @@ user_agent = {
     'User-Agent': env('USER_AGENT')
 }
 
-def pyowm_parser() -> None:
-    owm = OWM(API_KEY)
-    mgr = owm.weather_manager()
-    one_call = mgr.one_call(lat=52.5244, lon=13.4105)
-    for x in one_call.forecast_daily:
-        print(x)
+owm = OWM(API_KEY)
+mgr = owm.weather_manager()
+one_call = mgr.one_call(lat=52.5244, lon=13.4105)
 
 
-pyowm_parser()
+def current_weather():
+    return {
+        'Clouds': one_call.current.clouds,
+        'Humidity': one_call.current.humidity,
+        'Status': one_call.current.status,
+        'Detailed status': one_call.current.detailed_status,
+        'Visibility distance': one_call.current.visibility_distance,
+        'Temperature': one_call.current.temp
+    }
+
+
+print(current_weather())
+
 # if __name__ == '__main__':
 #     loop = get_event_loop()
 #     loop.run_until_complete(test())
