345709e61f430701a7f87fd7154937610f4b8c5e
Iglek Kuat <dizinnes228@gmail.com>
2020-10-03 23:13:14 +0600

    fix(weather): functions and delete a quotes in dict

--- weather/parser.py
+++ weather/parser.py
@@ -34,13 +34,13 @@ def forecast_daily() -> List:
     day = 1
     for x in range(0, 7):
         weather_dict = {
-            f'Weather information for day {day} ': {f'Clouds: {one_call.forecast_daily[x].clouds}',
-                                                    f'Humidity: {one_call.forecast_daily[x].humidity}',
-                                                    f'Status: {one_call.forecast_daily[x].status}',
-                                                    f'Detailed status: {one_call.forecast_daily[x].detailed_status}',
-                                                    f'Visibility distance: {one_call.forecast_daily[x].visibility_distance}',
-                                                    f'Temperature: {one_call.forecast_daily[x].temperature().get("day", None)}',
-                                                    f'Weather_icon_name: {one_call.forecast_daily[x].weather_icon_name}'},
+            'Clouds': one_call.forecast_daily[x].clouds,
+            'Humidity': one_call.forecast_daily[x].humidity,
+            'Status': one_call.forecast_daily[x].status,
+            'Detailed status': one_call.forecast_daily[x].detailed_status,
+            'Visibility distance': one_call.forecast_daily[x].visibility_distance,
+            'Temperature': one_call.forecast_daily[x].temperature().get("day", None),
+            'Weather_icon_name': one_call.forecast_daily[x].weather_icon_name,
         }
         day += 1
         weather.append(weather_dict)
--- weather/views.py
+++ weather/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
-from .parser import current_weather
+from .parser import current_weather, forecast_daily
 
 
 async def index(request):
-    weather_results: dict = current_weather()
+    weather_current_results: dict = current_weather()
+    weather_forecast = forecast_daily()
     results = {
-        'Clouds': weather_results['Clouds'],
-        'Humidity': weather_results['Humidity'],
-        'Status': weather_results['Status'],
-        'Detailed_status': weather_results['Detailed_status'],
-        'Visibility_distance': weather_results['Visibility_distance'],
-        'Temperature': weather_results['Temperature'],
-        'Weather_icon_name': weather_results['Weather_icon_name'],
+        # currently:
+        'Clouds': weather_current_results['Clouds'],
+        'Humidity': weather_current_results['Humidity'],
+        'Status': weather_current_results['Status'],
+        'Detailed_status': weather_current_results['Detailed_status'],
+        'Visibility_distance': weather_current_results['Visibility_distance'],
+        'Temperature': weather_current_results['Temperature'],
+        'Weather_icon_name': weather_current_results['Weather_icon_name'],
+        # forecast
+        'Clouds_forecast': weather_forecast[0]['Clouds']
     }
+
+    print()
+
     return render(request, 'weather/index.html', results)
