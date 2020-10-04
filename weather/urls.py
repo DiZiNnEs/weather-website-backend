a2e2ab13967f24a23f9a2b0eb21208c6318a27ee
Iglek Kuat <dizinnes228@gmail.com>
2020-10-04 17:07:17 +0600

    feat(weather): add forecast_weekly function in urls

--- weather/urls.py
+++ weather/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('weekly/', views.forecast_daily, name='weekly'),
 ]
