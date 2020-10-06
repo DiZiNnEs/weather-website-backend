30f85376d43d5d13a1881eb298af7a12728f93c8
Iglek Kuat <dizinnes228@gmail.com>
2020-10-06 15:46:57 +0600

    refactor(weather): change a url link

--- weather/urls.py
+++ weather/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('current/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('weekly/', views.forecast_weekly, name='weekly'),
 ]
