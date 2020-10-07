66be22ae2490224e7a115f3cd98aedd54459c904
Iglek Kuat <dizinnes228@gmail.com>
2020-10-07 13:27:32 +0600

    refactor(weather): update function and add return result in cycle

--- weather/views.py
+++ weather/views.py
@@ -24,15 +24,18 @@ async def index(request):
 async def forecast_weekly(request):
     weather_forecast = forecast_daily()
     results_list = []
-    results = {
-        # weekly
-        'Clouds_forecast': weather_forecast[0]['Clouds'],
-        'Humidity_forecast': weather_forecast[0]['Humidity'],
-        'Status_forecast': weather_forecast[0]['Status'],
-        'Detailed_status_forecast': weather_forecast[0]['Detailed status'],
-        'Visibility_distance_forecast': weather_forecast[0]['Visibility distance'],
-        'Temperature_forecast': weather_forecast[0]['Temperature'],
-        'Weather_icon_name_forecast': weather_forecast[0]['Weather_icon_name'],
-    }
 
-    return render(request, 'weather/weekly_forecast.html', weather_forecast)
+    for x in range(0, 7):
+        results = {
+            # weekly
+            'Clouds_forecast': weather_forecast[x]['Clouds'],
+            'Humidity_forecast': weather_forecast[x]['Humidity'],
+            'Status_forecast': weather_forecast[x]['Status'],
+            'Detailed_status_forecast': weather_forecast[x]['Detailed status'],
+            'Visibility_distance_forecast': weather_forecast[x]['Visibility distance'],
+            'Temperature_forecast': weather_forecast[x]['Temperature'],
+            'Weather_icon_name_forecast': weather_forecast[x]['Weather_icon_name'],
+        }
+        results_list.append(results)
+
+    return render(request, 'weather/weekly_forecast.html', results_list)
