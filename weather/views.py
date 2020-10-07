2f8cfe425f69ba8e9d764c7e6439956727b1f29c
Iglek Kuat <dizinnes228@gmail.com>
2020-10-08 01:37:47 +0600

    feat(weather): add minute forecast parser in views.py

--- weather/views.py
+++ weather/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .parser import (
     current_weather,
     forecast_daily_parser,
-    forecast_hourly_parser
+    forecast_hourly_parser,
+    forecast_minute_parser
 )
 
 
@@ -64,3 +65,25 @@ async def forecast_hourly(request):
     context = {'city_weather': results_list}
 
     return render(request, 'weather/hourly_forecast.html', context)
+
+
+async def forecast_minute(request):
+    weather_forecast = forecast_minute_parser()
+    results_list = []
+
+    for x in range(0, 7):
+        results = {
+            # weekly
+            'Clouds_forecast': weather_forecast[x]['Clouds'],
+            'Humidity_forecast': weather_forecast[x]['Humidity'],
+            'Status_forecast': weather_forecast[x]['Status'],
+            'Detailed_status_forecast': weather_forecast[x]['Detailed status'],
+            'Visibility_distance_forecast': weather_forecast[x]['Visibility distance'],
+            'Temperature_forecast': weather_forecast[x]['Temperature'],
+            'Weather_icon_name_forecast': weather_forecast[x]['Weather_icon_name'],
+        }
+        results_list.append(results)
+
+    context = {'city_weather': results_list}
+
+    return render(request, 'weather/minute_forecast.html', context)
