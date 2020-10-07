413c32301e7c1ebb1b710e15e4fd7e8d0c4eb87d
Iglek Kuat <dizinnes228@gmail.com>
2020-10-08 01:37:56 +0600

    feat(weather): add minute forecast parser in urls.py

--- weather/urls.py
+++ weather/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('weekly/', views.forecast_weekly, name='weekly'),
     path('hourly/', views.forecast_hourly, name='hourly'),
+    path('minute/', views.forecast_minute, name='minute'),
 ]
