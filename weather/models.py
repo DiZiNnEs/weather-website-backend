f79d29f1a6ff1e08c6d1403bc90555a58ec73440
Iglek Kuat <dizinnes228@gmail.com>
2020-10-09 14:40:54 +0600

    feat(weather): make migrate and add PostGIS

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.contrib.postgres.operations import CreateExtension
+from django.db import migrations
 
-# Create your models here.
+
+class Migration(migrations.Migration):
+    operations = [
+        CreateExtension('postgis'),
+        ...
+    ]
