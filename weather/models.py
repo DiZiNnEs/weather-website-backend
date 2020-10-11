0f474613ea1dadf1189ded132a8580557967a215
Iglek Kuat <dizinnes228@gmail.com>
2020-10-11 19:25:47 +0600

    feat(weather): add new field in database

--- weather/models.py
+++ weather/models.py
@@ -1,5 +1,5 @@
 from django.contrib.postgres.operations import CreateExtension
-from django.db import migrations
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
@@ -7,3 +7,13 @@ class Migration(migrations.Migration):
         CreateExtension('postgis'),
         ...
     ]
+
+
+class City(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'cites'
