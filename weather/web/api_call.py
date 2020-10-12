b54b2bf2e7cb5dbec746da52c7e03bdeae684c3a
Iglek Kuat <dizinnes228@gmail.com>
2020-10-12 20:03:19 +0600

    refactor(templates): add one function with conditions

--- weather/web/api_call.py
+++ weather/web/api_call.py
@@ -84,3 +84,69 @@ def forecast_minute_parser() -> List:
         weather.append(weather_dict)
 
     return weather
+
+
+def weather(weather_='current'):
+    weather_list = []
+    day = 1
+    if weather_ == 'current':
+        for x in range(0, 7):
+            weather_dict = {
+                'Clouds': one_call.forecast_minutely[x].clouds,
+                'Humidity': one_call.forecast_minutely[x].humidity,
+                'Status': one_call.forecast_minutely[x].status,
+                'Detailed status': one_call.forecast_minutely[x].detailed_status,
+                'Visibility distance': one_call.forecast_minutely[x].visibility_distance,
+                'Temperature': one_call.forecast_minutely[x].temperature().get("day", None),
+                'Weather_icon_name': one_call.forecast_minutely[x].weather_icon_name,
+            }
+            day += 1
+            weather_list.append(weather_dict)
+
+    elif weather_ == 'minute':
+        for x in range(0, 7):
+            weather_dict = {
+                'Clouds': one_call.forecast_minutely[x].clouds,
+                'Humidity': one_call.forecast_minutely[x].humidity,
+                'Status': one_call.forecast_minutely[x].status,
+                'Detailed status': one_call.forecast_minutely[x].detailed_status,
+                'Visibility distance': one_call.forecast_minutely[x].visibility_distance,
+                'Temperature': one_call.forecast_minutely[x].temperature().get("day", None),
+                'Weather_icon_name': one_call.forecast_minutely[x].weather_icon_name,
+            }
+            day += 1
+            weather_list.append(weather_dict)
+
+    elif weather_ == 'hourly':
+        for x in range(0, 7):
+            weather_dict = {
+                'Clouds': one_call.forecast_hourly[x].clouds,
+                'Humidity': one_call.forecast_hourly[x].humidity,
+                'Status': one_call.forecast_hourly[x].status,
+                'Detailed status': one_call.forecast_hourly[x].detailed_status,
+                'Visibility distance': one_call.forecast_hourly[x].visibility_distance,
+                'Temperature': one_call.forecast_hourly[x].temperature().get("day", None),
+                'Weather_icon_name': one_call.forecast_hourly[x].weather_icon_name,
+            }
+            day += 1
+            weather_list.append(weather_dict)
+
+    elif weather_ == 'daily':
+        for x in range(0, 7):
+            weather_dict = {
+                'Clouds': one_call.forecast_daily[x].clouds,
+                'Humidity': one_call.forecast_daily[x].humidity,
+                'Status': one_call.forecast_daily[x].status,
+                'Detailed status': one_call.forecast_daily[x].detailed_status,
+                'Visibility distance': one_call.forecast_daily[x].visibility_distance,
+                'Temperature': one_call.forecast_daily[x].temperature().get("day", None),
+                'Weather_icon_name': one_call.forecast_daily[x].weather_icon_name,
+            }
+            day += 1
+            weather_list.append(weather_dict)
+
+    return weather_list
+
+
+print(weather('hourly'))
+
