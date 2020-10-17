d5defd647bbdcd06b06ae6227404981244621e80
Iglek Kuat <dizinnes228@gmail.com>
2020-10-17 19:30:33 +0600

    refactor(weather): update indents and imports

--- weather/forms.py
+++ weather/forms.py
@@ -6,4 +6,6 @@ class CityForm(ModelForm):
     class Meta:
         model = City
         fields = ['name']
-        widgets = {'name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Entry city name', 'id': 'exampleInputEmail1', 'aria-describedby': 'emailHelp'})}
+        widgets = {'name': TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Entry city name', 'id': 'exampleInputEmail1',
+                   'aria-describedby': 'emailHelp'})}
