d2d689717d35f124f34cb03b2c06436fa1e5a831
Iglek Kuat <dizinnes228@gmail.com>
2020-10-17 19:35:06 +0600

    refactor(weather): change url name

--- weather/urls.py
+++ weather/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('all/', views.__weather_all, name='all'),
+    path('weather/', views.weather, name='all'),
     path('about/', views.about, name='about'),
     path('coordinates/', views.get_user_coordinates, name='get_user_coordinates'),
 ]
