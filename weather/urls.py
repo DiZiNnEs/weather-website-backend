0f6d4a0f0b8eee2074864cf62f69de825cbc3b86
Iglek Kuat <dizinnes228@gmail.com>
2020-10-17 19:26:39 +0600

    refactor(weather): delete the damn code

--- weather/urls.py
+++ weather/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('weekly/', views.forecast_daily, name='weekly'),
-    path('hourly/', views.forecast_hourly, name='hourly'),
-    path('minute/', views.forecast_minute, name='minute'),
     path('all/', views.__weather_all, name='all'),
     path('about/', views.about, name='about'),
     path('coordinates/', views.get_user_coordinates, name='get_user_coordinates'),
