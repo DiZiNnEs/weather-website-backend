4770ecb9069483649f8cef7695352e151eb2bd4f
Iglek Kuat <dizinnes228@gmail.com>
2020-10-17 22:03:49 +0600

    refactor(weather): delete unused and fuck code

--- weather/views.py
+++ weather/views.py
@@ -7,11 +7,9 @@ from .web import geoapi
 
 
 async def index(request):
-    weather_current_results: dict = weather_current('current')
-    form = CityForm()
+    weather_current_results: dict = weather_current()
 
     results = {
-        # currently:
         'Clouds': weather_current_results['Clouds'],
         'Humidity': weather_current_results['Humidity'],
         'Status': weather_current_results['Status'],
@@ -19,13 +17,45 @@ async def index(request):
         'Visibility_distance': weather_current_results['Visibility_distance'],
         'Temperature': weather_current_results['Temperature'],
         'Weather_icon_name': weather_current_results['Weather_icon_name'],
-        'form': form,
     }
 
-    if request.method == 'POST':
-        print(request.POST)
-
     return render(request, 'weather/index.html', results)
+
+
+def weather(request):
+    call_weather = weather_all()
+    results_list = []
+    for get_weather_information in range(0, 7):
+        results = {
+            'Clouds_daily': call_weather[get_weather_information]['Clouds_daily'],
+            'Humidity_daily': call_weather[get_weather_information]['Humidity_daily'],
+            'Status_daily': call_weather[get_weather_information]['Status_daily'],
+            'Detailed_status_daily': call_weather[get_weather_information]['Detailed status_daily'],
+            'Visibility_distance_forecast_daily': call_weather[get_weather_information]['Visibility distance_daily'],
+            'Temperature_forecast_daily': call_weather[get_weather_information]['Temperature_daily'],
+            'Weather_icon_name_forecast_daily': call_weather[get_weather_information]['Weather_icon_name_daily'],
+
+            'Clouds_hourly': call_weather[get_weather_information]['Clouds_hourly'],
+            'Humidity_hourly': call_weather[get_weather_information]['Humidity_hourly'],
+            'Status_hourly': call_weather[get_weather_information]['Status_hourly'],
+            'Detailed_status_hourly': call_weather[get_weather_information]['Detailed status_hourly'],
+            'Visibility_distance_forecast_hourly': call_weather[get_weather_information]['Visibility distance_hourly'],
+            'Temperature_forecast_hourly': call_weather[get_weather_information]['Temperature_hourly'],
+            'Weather_icon_name_forecast_hourly': call_weather[get_weather_information]['Weather_icon_name_hourly'],
+
+            'Clouds_minutely': call_weather[get_weather_information]['Clouds_minutely'],
+            'Humidity_minutely': call_weather[get_weather_information]['Humidity_minutely'],
+            'Status_minutely': call_weather[get_weather_information]['Status_minutely'],
+            'Detailed_status_minutely': call_weather[get_weather_information]['Detailed status_minutely'],
+            'Visibility_distance_forecast_minutely': call_weather[get_weather_information][
+                'Visibility distance_minutely'],
+            'Temperature_forecast_minutely': call_weather[get_weather_information]['Temperature_minutely'],
+            'Weather_icon_name_forecast_minutely': call_weather[get_weather_information]['Weather_icon_name_minutely'],
+        }
+        results_list.append(results)
+    context = {'city_weather': results_list}
+
+    return render(request, 'weather/weather.html', context)
 
 
 def get_user_coordinates(request):
@@ -47,38 +77,3 @@ def get_user_coordinates(request):
 
 def about(request):
     return render(request, 'weather/about.html')
-
-
-def weather(request):
-    call_weather = weather_all()
-    results_list = []
-    for x in range(0, 7):
-        results = {
-            'Clouds_daily': call_weather[x]['Clouds_daily'],
-            'Humidity_daily': call_weather[x]['Humidity_daily'],
-            'Status_daily': call_weather[x]['Status_daily'],
-            'Detailed_status_daily': call_weather[x]['Detailed status_daily'],
-            'Visibility_distance_forecast_daily': call_weather[x]['Visibility distance_daily'],
-            'Temperature_forecast_daily': call_weather[x]['Temperature_daily'],
-            'Weather_icon_name_forecast_daily': call_weather[x]['Weather_icon_name_daily'],
-
-            'Clouds_hourly': call_weather[x]['Clouds_hourly'],
-            'Humidity_hourly': call_weather[x]['Humidity_hourly'],
-            'Status_hourly': call_weather[x]['Status_hourly'],
-            'Detailed_status_hourly': call_weather[x]['Detailed status_hourly'],
-            'Visibility_distance_forecast_hourly': call_weather[x]['Visibility distance_hourly'],
-            'Temperature_forecast_hourly': call_weather[x]['Temperature_hourly'],
-            'Weather_icon_name_forecast_hourly': call_weather[x]['Weather_icon_name_hourly'],
-
-            'Clouds_minutely': call_weather[x]['Clouds_minutely'],
-            'Humidity_minutely': call_weather[x]['Humidity_minutely'],
-            'Status_minutely': call_weather[x]['Status_minutely'],
-            'Detailed_status_minutely': call_weather[x]['Detailed status_minutely'],
-            'Visibility_distance_forecast_minutely': call_weather[x]['Visibility distance_minutely'],
-            'Temperature_forecast_minutely': call_weather[x]['Temperature_minutely'],
-            'Weather_icon_name_forecast_minutely': call_weather[x]['Weather_icon_name_minutely'],
-        }
-        results_list.append(results)
-    context = {'city_weather': results_list}
-
-    return render(request, 'weather/weather.html', context)
--- weather/web/api_call.py
+++ weather/web/api_call.py
@@ -17,77 +17,16 @@ mgr = owm.weather_manager()
 one_call = mgr.one_call(lat=52.5244, lon=13.4105)
 
 
-def weather_current(weather_: str) -> Dict or List:
-    weather_list = []
-    day = 1
-    if weather_ == 'current':
-        return {
-            'Clouds': one_call.current.clouds,
-            'Humidity': one_call.current.humidity,
-            'Status': one_call.current.status,
-            'Detailed_status': one_call.current.detailed_status,
-            'Visibility_distance': one_call.current.visibility_distance,
-            'Temperature': one_call.current.temp.get('temp', None),
-            'Weather_icon_name': one_call.current.weather_icon_name,
-        }
-
-    elif weather_ == 'minute':
-        for x in range(0, 7):
-            weather_dict = {
-                'Clouds': one_call.forecast_minutely[x].clouds,
-                'Humidity': one_call.forecast_minutely[x].humidity,
-                'Status': one_call.forecast_minutely[x].status,
-                'Detailed status': one_call.forecast_minutely[x].detailed_status,
-                'Visibility distance': one_call.forecast_minutely[x].visibility_distance,
-                'Temperature': one_call.forecast_minutely[x].temperature().get("day", None),
-                'Weather_icon_name': one_call.forecast_minutely[x].weather_icon_name,
-            }
-            day += 1
-            weather_list.append(weather_dict)
-
-    elif weather_ == 'hourly':
-        for x in range(0, 7):
-            weather_dict = {
-                'Clouds': one_call.forecast_hourly[x].clouds,
-                'Humidity': one_call.forecast_hourly[x].humidity,
-                'Status': one_call.forecast_hourly[x].status,
-                'Detailed status': one_call.forecast_hourly[x].detailed_status,
-                'Visibility distance': one_call.forecast_hourly[x].visibility_distance,
-                'Temperature': one_call.forecast_hourly[x].temperature().get("day", None),
-                'Weather_icon_name': one_call.forecast_hourly[x].weather_icon_name,
-            }
-            day += 1
-            weather_list.append(weather_dict)
-
-    elif weather_ == 'daily':
-        for x in range(0, 7):
-            weather_dict = {
-                'Clouds': one_call.forecast_daily[x].clouds,
-                'Humidity': one_call.forecast_daily[x].humidity,
-                'Status': one_call.forecast_daily[x].status,
-                'Detailed status': one_call.forecast_daily[x].detailed_status,
-                'Visibility distance': one_call.forecast_daily[x].visibility_distance,
-                'Temperature': one_call.forecast_daily[x].temperature().get("day", None),
-                'Weather_icon_name': one_call.forecast_daily[x].weather_icon_name,
-            }
-            day += 1
-            weather_list.append(weather_dict)
-
-    elif weather_ == 'weekly':
-        for x in range(0, 7):
-            weather_dict = {
-                'Clouds': one_call.forecast_daily[x].clouds,
-                'Humidity': one_call.forecast_daily[x].humidity,
-                'Status': one_call.forecast_daily[x].status,
-                'Detailed status': one_call.forecast_daily[x].detailed_status,
-                'Visibility distance': one_call.forecast_daily[x].visibility_distance,
-                'Temperature': one_call.forecast_daily[x].temperature().get("day", None),
-                'Weather_icon_name': one_call.forecast_daily[x].weather_icon_name,
-            }
-            day += 1
-            weather_list.append(weather_dict)
-
-    return weather_list
+def weather_current() -> Dict or List:
+    return {
+        'Clouds': one_call.current.clouds,
+        'Humidity': one_call.current.humidity,
+        'Status': one_call.current.status,
+        'Detailed_status': one_call.current.detailed_status,
+        'Visibility_distance': one_call.current.visibility_distance,
+        'Temperature': one_call.current.temp.get('temp', None),
+        'Weather_icon_name': one_call.current.weather_icon_name,
+    }
 
 
 def weather_all() -> Dict or List:
