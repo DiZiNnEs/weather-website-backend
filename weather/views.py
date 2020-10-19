d6470743f2b4f121aba5d35dc4dafb51828d6ff4
Iglek Kuat <dizinnes228@gmail.com>
2020-10-19 19:04:59 +0600

    feat(weather): add new weather phenomenon in views.py

--- weather/views.py
+++ weather/views.py
@@ -46,6 +46,21 @@ def weather(request):
             'Visibility_distance_forecast_daily': call_weather[get_weather_information]['Visibility distance_daily'],
             'Temperature_forecast_daily': call_weather[get_weather_information]['Temperature_daily'],
             'Weather_icon_name_forecast_daily': call_weather[get_weather_information]['Weather_icon_name_daily'],
+            #  new daily
+            'Heat_index_daily': call_weather[get_weather_information]['Heat_index_daily'],
+            'Pressure_daily': call_weather[get_weather_information]['Pressure_daily'],
+            'Rain_daily': call_weather[get_weather_information]['Rain_daily'],
+            'Ref_time_daily': call_weather[get_weather_information]['Ref_time_daily'],
+            'Snow_daily': call_weather[get_weather_information]['Snow_daily'],
+            'Srise_time_daily': call_weather[get_weather_information]['Srise_time_daily'],
+            'sset_time_daily': call_weather[get_weather_information]['sset_time_daily'],
+            'Utc_offset_daily': call_weather[get_weather_information]['Utc_offset_daily'],
+            'Uvi_daily': call_weather[get_weather_information]['Uvi_daily'],
+            'Detailed status_daily': call_weather[get_weather_information]['Detailed status_daily'],
+            'Visibility distance_daily': call_weather[get_weather_information]['Visibility distance_daily'],
+            'Temperature_daily': call_weather[get_weather_information]['Temperature_daily'],
+            'Wind_daily': call_weather[get_weather_information]['Wind_daily'],
+
 
             'Clouds_hourly': call_weather[get_weather_information]['Clouds_hourly'],
             'Humidity_hourly': call_weather[get_weather_information]['Humidity_hourly'],
@@ -54,6 +69,21 @@ def weather(request):
             'Visibility_distance_forecast_hourly': call_weather[get_weather_information]['Visibility distance_hourly'],
             'Temperature_forecast_hourly': call_weather[get_weather_information]['Temperature_hourly'],
             'Weather_icon_name_forecast_hourly': call_weather[get_weather_information]['Weather_icon_name_hourly'],
+            #  new hourly
+            'Heat_index_hourly': call_weather[get_weather_information]['Heat_index_hourly'],
+            'Pressure_hourly': call_weather[get_weather_information]['Pressure_hourly'],
+            'Rain_hourly': call_weather[get_weather_information]['Rain_hourly'],
+            'Ref_time_hourly': call_weather[get_weather_information]['Ref_time_hourly'],
+            'Snow_hourly': call_weather[get_weather_information]['Snow_hourly'],
+            'Srise_time_hourly': call_weather[get_weather_information]['Srise_time_hourly'],
+            'sset_time_hourly': call_weather[get_weather_information]['sset_time_hourly'],
+            'Utc_offset_hourly': call_weather[get_weather_information]['Utc_offset_hourly'],
+            'Uvi_hourly': call_weather[get_weather_information]['Uvi_hourly'],
+            'Detailed status_hourly': call_weather[get_weather_information]['Detailed status_hourly'],
+            'Visibility distance_hourly': call_weather[get_weather_information]['Visibility distance_hourly'],
+            'Temperature_hourly': call_weather[get_weather_information]['Temperature_hourly'],
+            'Wind_hourly': call_weather[get_weather_information]['Wind_hourly'],
+
 
             'Clouds_minutely': call_weather[get_weather_information]['Clouds_minutely'],
             'Humidity_minutely': call_weather[get_weather_information]['Humidity_minutely'],
@@ -63,6 +93,20 @@ def weather(request):
                 'Visibility distance_minutely'],
             'Temperature_forecast_minutely': call_weather[get_weather_information]['Temperature_minutely'],
             'Weather_icon_name_forecast_minutely': call_weather[get_weather_information]['Weather_icon_name_minutely'],
+            #  new minutely
+            'Heat_index_minutely': call_weather[get_weather_information]['Heat_index_minutely'],
+            'Pressure_minutely': call_weather[get_weather_information]['Pressure_minutely'],
+            'Rain_minutely': call_weather[get_weather_information]['Rain_minutely'],
+            'Ref_time_minutely': call_weather[get_weather_information]['Ref_time_minutely'],
+            'Snow_minutely': call_weather[get_weather_information]['Snow_minutely'],
+            'Srise_time_minutely': call_weather[get_weather_information]['Srise_time_minutely'],
+            'sset_time_minutely': call_weather[get_weather_information]['sset_time_minutely'],
+            'Utc_offset_minutely': call_weather[get_weather_information]['Utc_offset_minutely'],
+            'Uvi_minutely': call_weather[get_weather_information]['Uvi_minutely'],
+            'Detailed status_minutely': call_weather[get_weather_information]['Detailed status_minutely'],
+            'Visibility distance_minutely': call_weather[get_weather_information]['Visibility distance_minutely'],
+            'Temperature_minutely': call_weather[get_weather_information]['Temperature_minutely'],
+            'Wind_minutely': call_weather[get_weather_information]['Wind_minutely'],
         }
         results_list.append(results)
     context = {'city_weather': results_list}
